e1a6120cafc5a6ffe391cd423692077d60f86ed9
Luís Martinho <lmartinho@gmail.com>
2018-11-04 10:58:00 +0000

    Start creating new features

--- Titanic.R
+++ Titanic.R
@@ -2,16 +2,83 @@
 train <- read.csv("train.csv", header = TRUE)
 test <- read.csv("test.csv", header = TRUE)
 
+# Add blank labels to the test set
 test.survived <- data.frame(Survived = rep("None", nrow(test)), test[,])
 
+# Merge sets into a bigger one
 data.combined <- rbind(train, test.survived)
 
+# Start learning more about the data
 str(data.combined)
 
+# Machine learning algorithms favor discrete values, so use R's factors (enum)
 # Convert chr to factor
 data.combined$Survived <- as.factor(data.combined$Survived)
 # Convert int to factor
 data.combined$Pclass <- as.factor(data.combined$Pclass)
 
-# Gross Survival Rates
+# Take a look at Gross Survival Rates
 table(data.combined$Survived)
+
+# Take a look at Pclass
+table(data.combined$Pclass)
+
+library(ggplot2)
+
+train$Pclass <- as.factor(train$Pclass)
+ggplot(train, aes(x = Pclass, fill = factor(Survived))) + 
+  geom_bar(width=0.5) + 
+  xlab("Pclass") + 
+  ylab("Total Count") + 
+  labs(fill = "Survived")
+
+head(as.character(train$Name))
+
+length(unique(as.character(data.combined$Name)))
+
+dup.names <- as.character(data.combined[which(duplicated(as.character(data.combined$Name))), "Name"])
+
+data.combined[which(data.combined$Name %in% dup.names),]
+
+library(stringr)
+
+# Any correlation with other variables (e.g., sibsp)?
+misses <- data.combined[which(str_detect(data.combined$Name, "Miss.")),]
+misses[1:5,]
+
+# Hypothesis - Name titles correlate with age
+mrses <- data.combined[which(str_detect(data.combined$Name, "Mrs.")),]
+mrses[1:5,]
+
+males = data.combined[which(train$Sex == 'male'),]
+males[1:5,]
+
+# Start doing some feature engineering (new Title feature)
+extractTitle <- function(name) {
+  name = as.character(name)
+  
+  if (length(grep("Miss.", name)) > 0) {
+    return ("Miss.")
+  } else if (length(grep("Master.", name)) > 0) {
+    return ("Master.")
+  } else if (length(grep("Mrs.", name)) > 0) {
+    return ("Mrs.")
+  } else if (length(grep("Mr.", name)) > 0) {
+    return ("Mr.")
+  } else {
+    return ("Other")
+  }
+}
+titles <- NULL
+for (i in 1:nrow(data.combined)) {
+  titles <- c(titles, extractTitle(data.combined[i, "Name"]))
+}
+data.combined$Title <- as.factor(titles)
+
+ggplot(data.combined[1:891,], aes(x = Title, fill = Survived)) +
+  geom_bar(width = 0.5) + 
+  facet_wrap(~Pclass) + 
+  ggtitle("Pclass") +
+  xlab("Title") + 
+  ylab("Total Count") + 
+  labs(fill = "Survived")
